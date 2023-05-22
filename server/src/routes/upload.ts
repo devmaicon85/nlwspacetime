2e25b3d247743c48d3929e6d022feaccdc3b94c7
Maicon <devmaicon@gmail.com>
2023-05-22 12:57:57 -0300

    update upload add trycath

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -9,37 +9,41 @@ const pump = promisify(pipeline)
 
 export async function uploadRoutes(app: FastifyInstance) {
   app.post('/upload', async (request, reply) => {
-    const upload = await request.file({
-      limits: {
-        fileSize: 5_242_880, // 5MB
-      },
-    })
+    try {
+      const upload = await request.file({
+        limits: {
+          fileSize: 5_242_880, // 5MB
+        },
+      })
 
-    if (!upload) {
-      return reply.status(400).send()
+      if (!upload) {
+        return reply.status(400).send()
+      }
+      const mimiTypeRegex = /^(image|video)\/[a-zA-z]+/
+
+      const isValidFileFormat = mimiTypeRegex.test(upload.mimetype)
+
+      if (!isValidFileFormat) {
+        return reply.status(400).send()
+      }
+
+      const fileId = randomUUID()
+      const extension = extname(upload.filename)
+
+      const fileName = fileId.concat(extension)
+
+      const writeStream = createWriteStream(
+        resolve(__dirname, '../../uploads', fileName),
+      )
+
+      await pump(upload.file, writeStream)
+
+      const fullUrl = request.protocol.concat('://').concat(request.hostname)
+      const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+
+      return { fileUrl }
+    } catch (error) {
+      console.log('🚀 ~ file: upload.ts:48 ~ app.post ~ error:', error)
     }
-    const mimiTypeRegex = /^(image|video)\/[a-zA-z]+/
-
-    const isValidFileFormat = mimiTypeRegex.test(upload.mimetype)
-
-    if (!isValidFileFormat) {
-      return reply.status(400).send()
-    }
-
-    const fileId = randomUUID()
-    const extension = extname(upload.filename)
-
-    const fileName = fileId.concat(extension)
-
-    const writeStream = createWriteStream(
-      resolve(__dirname, '../../uploads', fileName),
-    )
-
-    await pump(upload.file, writeStream)
-
-    const fullUrl = request.protocol.concat('://').concat(request.hostname)
-    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
-
-    return { fileUrl }
   })
 }
